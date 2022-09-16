d9751f227596dbf6ba9a355aa5487eec4cfcd841
Federico Luzzi <fedeluzzi00@gmail.com>
2022-09-16 13:45:36 +0100

    chore(frontend): dedicated shared dependencies key in package.json to control module federation

--- packages/frontend/config-overrides.js
+++ packages/frontend/config-overrides.js
@@ -3,6 +3,16 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
 const { ModuleFederationPlugin } = require('webpack').container
+const pkg = require('./package.json')
+
+const sharedDependencies = Object.entries(pkg.sharedDependencies).reduce((deps, [name, version]) => {
+  deps[name] = {
+    requiredVersion: version,
+    singleton: true,
+    eager: false,
+  }
+  return deps
+})
 
 // Used to make the build reproducible between different machines (IPFS-related)
 module.exports = (config, env) => {
@@ -11,12 +21,7 @@ module.exports = (config, env) => {
     new ModuleFederationPlugin({
       name: 'home',
       filename: 'entry.js',
-      shared: [
-        {
-          react: { singleton: true },
-          'react-dom': { singleton: true },
-        },
-      ],
+      shared: sharedDependencies,
     })
   )
   if (env !== 'production') {
