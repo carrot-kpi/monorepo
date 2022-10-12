4f22d18791134127db38fa3f9c9886ecc1f09ca4
Federico Luzzi <fedeluzzi00@gmail.com>
2022-10-12 17:18:25 +0100

    feat(frontend): use new carrot core provider

--- packages/frontend/src/bootstrap.tsx
+++ packages/frontend/src/bootstrap.tsx
@@ -3,12 +3,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { App } from './pages/app'
-import { configureChains, chain as wagmiChain, createClient, WagmiConfig } from 'wagmi'
+import { chain as wagmiChain, Chain } from 'wagmi'
 import { infuraProvider } from 'wagmi/providers/infura'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { ChainId, INFURA_PROJECT_ID } from '@carrot-kpi/sdk'
-import { initializeI8n } from '@carrot-kpi/react'
+import { CarrotCoreProvider } from '@carrot-kpi/react'
 import i18n from 'i18next'
 import { resources } from './i18n'
 import { CarrotUIProvider } from '@carrot-kpi/ui'
@@ -17,39 +17,34 @@ const supportedChains = Object.values(wagmiChain).filter((chain) => {
   return chain.id in ChainId
 })
 
-const { provider, chains, webSocketProvider } = configureChains(supportedChains, [
-  infuraProvider({ apiKey: INFURA_PROJECT_ID }),
-])
-
-const client = createClient({
-  autoConnect: true,
-  connectors: [
-    new InjectedConnector({ chains }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        qrcode: true,
-      },
-    }),
-  ],
-  provider,
-  webSocketProvider,
-})
+const getConnectors = (chains: Chain[]) => [
+  new InjectedConnector({ chains }),
+  new WalletConnectConnector({
+    chains,
+    options: {
+      qrcode: true,
+    },
+  }),
+]
 
 __webpack_init_sharing__('default')
-
-initializeI8n(i18n, resources)
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(
   <StrictMode>
     <HashRouter>
-      <WagmiConfig client={client}>
+      <CarrotCoreProvider
+        i18nInstance={i18n}
+        i18nResources={resources}
+        supportedChains={supportedChains}
+        providers={[infuraProvider({ apiKey: INFURA_PROJECT_ID })]}
+        getConnectors={getConnectors}
+      >
         <CarrotUIProvider>
           <App />
         </CarrotUIProvider>
-      </WagmiConfig>
+      </CarrotCoreProvider>
     </HashRouter>
   </StrictMode>
 )
