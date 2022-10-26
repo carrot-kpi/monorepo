9e6e722d52a58cdc022013fe5e51c29ed235de40
Federico Luzzi <fedeluzzi00@gmail.com>
2022-10-26 15:45:00 +0100

    feat(react): new specification uploader implementation for ipfs

--- packages/react/src/hooks/useDecentralizedStorageUploader.ts
+++ packages/react/src/hooks/useDecentralizedStorageUploader.ts
@@ -1,21 +1,45 @@
-import { Web3Storage } from 'web3.storage'
+import { KpiTokenSpecification } from '@carrot-kpi/sdk'
 
-export type DecentralizedStorage = 'ipfs'
-export type DecentralizedStorageUploader = (content: string) => Promise<void>
+export type DecentralizedStorageOption = 'ipfs'
+export type SpecificationUploader = (
+  specification: KpiTokenSpecification
+) => Promise<string>
 
-const web3Storage = new Web3Storage({
-  token:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNBNzU3RjVlNzUyRUE1M2IwYTE1QzA3MDJhODcyMTFjOUJGQTM5N0QiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDE4NDI5NTIzOTEsIm5hbWUiOiJqb2x0LXByZXZpZXctYnVpbGRzIn0.GMPv4WC8q-CIyCRjkIgG2EV2DnmD10bXd7sZoMcEVrs',
-})
+const WEB3_STORAGE_API_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNBNzU3RjVlNzUyRUE1M2IwYTE1QzA3MDJhODcyMTFjOUJGQTM5N0QiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDE4NDI5NTIzOTEsIm5hbWUiOiJqb2x0LXByZXZpZXctYnVpbGRzIn0.GMPv4WC8q-CIyCRjkIgG2EV2DnmD10bXd7sZoMcEVrs'
 
-const UPLOADER: Record<DecentralizedStorage, DecentralizedStorageUploader> = {
-  ipfs: async (content: string) => {
-    await web3Storage.put([new File([content], 'spec', { type: 'text/plain' })])
-  },
-}
+const SPECIFICATION_UPLOADER: Record<DecentralizedStorageOption, SpecificationUploader> =
+  {
+    ipfs: async (
+      specification: Omit<KpiTokenSpecification, 'ipfsHash'>
+    ): Promise<string> => {
+      const formData = new FormData()
+      formData.append(
+        'file',
+        new File([JSON.stringify(specification)], 'specification', {
+          type: 'text/plain',
+        })
+      )
+      const response = await fetch('https://api.web3.storage/upload', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${WEB3_STORAGE_API_TOKEN}`,
+          'X-Client': 'carrot-kpi/js',
+        },
+        body: formData,
+      })
+      if (!response.ok)
+        throw new Error(
+          `could not upload file to ipfs, request failed with status ${response.status}`
+        )
+      const { cid } = (await response.json()) as { cid: string }
+      return cid
+    },
+  }
 
 export const useDecentralizedStorageUploader = (
-  decentralizedStorage: DecentralizedStorage
-): DecentralizedStorageUploader => {
-  return UPLOADER[decentralizedStorage]
+  decentralizedStorage: DecentralizedStorageOption
+): SpecificationUploader => {
+  return SPECIFICATION_UPLOADER[decentralizedStorage]
 }
