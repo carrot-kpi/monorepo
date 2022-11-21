35c6bbd15bbbf7615b4ee180168945115dcfe898
Federico Luzzi <fedeluzzi00@gmail.com>
2022-11-21 15:29:57 +0000

    feat(react): updated i18n handling

--- packages/react/src/providers/core-provider/index.tsx
+++ packages/react/src/providers/core-provider/index.tsx
@@ -1,22 +1,20 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import {
   Chain,
   ChainProviderFn,
+  Client,
   configureChains,
   Connector,
   createClient,
   WagmiConfig,
 } from 'wagmi'
 import { ReactNode } from 'react'
-import { i18n } from 'i18next'
-import { initReactI18next } from 'react-i18next'
 import { resources } from '../../i18n/resources'
-import { CARROT_KPI_REACT_I18N_NAMESPACE } from '../../i18n'
+import { CARROT_KPI_REACT_I18N_NAMESPACE, i18next } from '../../i18n'
 import { IpfsService } from '@carrot-kpi/sdk'
 
 interface CarrotCoreProviderProps {
   children: ReactNode
-  i18nInstance: i18n
   i18nResources: {
     [languageCode: string]: { [namespace: string]: { [key: string]: string } }
   }
@@ -29,7 +27,6 @@ interface CarrotCoreProviderProps {
 
 export const CarrotCoreProvider = ({
   children,
-  i18nInstance,
   i18nResources,
   i18nDefaultNamespace,
   supportedChains,
@@ -37,33 +34,37 @@ export const CarrotCoreProvider = ({
   getConnectors,
   ipfsGateway,
 }: CarrotCoreProviderProps) => {
-  const { provider, chains, webSocketProvider } = configureChains(
-    supportedChains,
-    providers
-  )
+  const [client, setClient] = useState<Client<any, any> | null>(null)
 
-  const client = createClient({
-    autoConnect: true,
-    connectors: getConnectors(chains),
-    provider,
-    webSocketProvider,
-  })
+  useEffect(() => {
+    const { provider, chains, webSocketProvider } = configureChains(
+      supportedChains,
+      providers
+    )
+    setClient(
+      createClient({
+        autoConnect: true,
+        connectors: getConnectors(chains),
+        provider,
+        webSocketProvider,
+      })
+    )
+  }, [getConnectors, providers, supportedChains])
 
-  i18nInstance.use(initReactI18next).init({
-    resources: i18nResources,
-    // TODO: remove once a language detector is used
-    lng: 'en',
-    defaultNS: i18nDefaultNamespace,
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
-    },
-  })
-  Object.entries(resources).forEach(([language, keys]) => {
-    i18nInstance.addResourceBundle(language, CARROT_KPI_REACT_I18N_NAMESPACE, keys)
-  })
+  useEffect(() => {
+    Object.entries(i18nResources).forEach(([language, keys]) => {
+      i18next.addResourceBundle(language, i18nDefaultNamespace, keys)
+    })
+    Object.entries(resources).forEach(([language, keys]) => {
+      i18next.addResourceBundle(language, CARROT_KPI_REACT_I18N_NAMESPACE, keys)
+    })
+  }, [i18nDefaultNamespace, i18nResources])
 
+  if (!client) return <></>
   if (!!ipfsGateway) IpfsService.gateway = ipfsGateway
-
-  return <WagmiConfig client={client}>{children}</WagmiConfig>
+  return (
+    <WagmiConfig client={client}>
+      <Suspense fallback="Loading">{children}</Suspense>
+    </WagmiConfig>
+  )
 }
