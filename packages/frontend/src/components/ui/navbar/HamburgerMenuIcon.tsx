f3a2f9203cdd0427ddaeb2e588df931c243d4d88
Federico Luzzi <fedeluzzi00@gmail.com>
2022-12-12 16:44:58 +0000

    chore(frontend): format changes

--- packages/frontend/src/components/ui/navbar/HamburgerMenuIcon.tsx
+++ packages/frontend/src/components/ui/navbar/HamburgerMenuIcon.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-export const HamburgerMenuIcon = ({ color = 'black' }: { color?: string }) => (
-  <svg
-    width="36"
-    height="36"
-    viewBox="0 0 36 36"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <rect x="4.5" y="7.14258" width="27" height="2" fill={color} />
-    <rect x="4.5" y="26.8574" width="27" height="2" fill={color} />
-    <rect x="4.5" y="17" width="27" height="2" fill={color} />
-  </svg>
-)
+export const HamburgerMenuIcon = ({ color = "black" }: { color?: string }) => (
+    <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <rect x="4.5" y="7.14258" width="27" height="2" fill={color} />
+        <rect x="4.5" y="26.8574" width="27" height="2" fill={color} />
+        <rect x="4.5" y="17" width="27" height="2" fill={color} />
+    </svg>
+);
