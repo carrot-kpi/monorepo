359335de627074e08ba8c3f7cb68656cf52ea0a9
Federico Luzzi <fedeluzzi00@gmail.com>
2022-12-15 19:52:45 +0000

    fix(ui): move font import from provider to storybook preview

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,5 +1,8 @@
 import { CarrotUIProvider } from '../src/provider'
 
+import '@fontsource/ibm-plex-mono/400.css'
+import '@fontsource/ibm-plex-mono/500.css'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
--- packages/ui/src/provider/index.tsx
+++ packages/ui/src/provider/index.tsx
@@ -1,9 +1,6 @@
 import React, { ReactNode } from 'react'
 
 import '../global.css'
-// importing required fonts
-import '@fontsource/ibm-plex-mono/400.css'
-import '@fontsource/ibm-plex-mono/500.css'
 
 interface CarrotUIProviderProps {
   children: ReactNode
