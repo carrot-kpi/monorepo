642de92f1da2eb2ef7be55ffe72a5cd61b4da529
Federico Luzzi <fedeluzzi00@gmail.com>
2022-12-16 09:29:38 +0000

    fix(frontend): add ui library styles import to entrypoint

--- packages/frontend/src/bootstrap.tsx
+++ packages/frontend/src/bootstrap.tsx
@@ -14,6 +14,7 @@ import "./global.css";
 
 import "@fontsource/ibm-plex-mono/400.css";
 import "@fontsource/ibm-plex-mono/500.css";
+import "@carrot-kpi/ui/styles.css";
 
 const INFURA_PROJECT_ID = "0ebf4dd05d6740f482938b8a80860d13";
 
