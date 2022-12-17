90a216564284cd78988a16fccd27a867ba2ae1e6
Federico Luzzi <fedeluzzi00@gmail.com>
2022-12-17 16:40:21 +0000

    fix(react): remove some ts non null assertions and warning suppressions

--- packages/react/src/hooks/useNativeCurrency.ts
+++ packages/react/src/hooks/useNativeCurrency.ts
@@ -5,8 +5,6 @@ import { mainnet } from "wagmi/chains";
 export function useNativeCurrency(): Currency {
     const { chain } = useNetwork();
     // fallback to ether if chain id is not defined
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    if (!chain) return mainnet.nativeCurrency!;
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return chain.nativeCurrency!;
+    if (!chain) return mainnet.nativeCurrency;
+    return chain.nativeCurrency;
 }
