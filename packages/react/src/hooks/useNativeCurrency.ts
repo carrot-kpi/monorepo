bb27c90b3e4810b8d13243253cadac8275609e58
Federico Luzzi <fedeluzzi00@gmail.com>
2022-12-17 16:39:30 +0000

    fix(react): fix some wagmi imports

--- packages/react/src/hooks/useNativeCurrency.ts
+++ packages/react/src/hooks/useNativeCurrency.ts
@@ -1,11 +1,12 @@
 import { Currency } from "@carrot-kpi/sdk";
-import { useNetwork, chain as wagmiChain } from "wagmi";
+import { useNetwork } from "wagmi";
+import { mainnet } from "wagmi/chains";
 
 export function useNativeCurrency(): Currency {
     const { chain } = useNetwork();
     // fallback to ether if chain id is not defined
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    if (!chain) return wagmiChain.mainnet.nativeCurrency!;
+    if (!chain) return mainnet.nativeCurrency!;
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     return chain.nativeCurrency!;
 }
