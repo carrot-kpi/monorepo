4f418ac0af217d4c6bd6440c4acbb60455ae555b
Federico Luzzi <fedeluzzi00@gmail.com>
2023-01-03 15:52:28 +0000

    chore: remove ipfs pinner package (moved to its own repo)

--- packages/ipfs-pinner/src/utils.rs
+++
@@ -1,7 +0,0 @@
-use ethers::types::Address;
-
-pub fn get_address_from_str(address: &str, error_message: Option<&str>) -> Address {
-    address
-        .parse::<Address>()
-        .expect(error_message.unwrap_or("couldn't parse address"))
-}
