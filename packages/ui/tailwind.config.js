66e39ac0467ea975801be5ccf3d4add01864334e
Diogo Ferreira <diogomf@hey.com>
2023-01-04 02:29:42 -0600

    Fix: tailwind extend with prefix not exporting classNames (#33)
    * fix: text-2xs not exporting className from ui
    
    * fix: rename fontsize from 2xs to xxs

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -3,4 +3,11 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     presets: ["./tailwind.preset.js"],
     prefix: "cui-",
+    theme: {
+        extend: {
+            fontSize: {
+                xxs: "10px",
+            },
+        },
+    },
 };
