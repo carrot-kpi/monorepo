62e8a126353e86d338ec5b2bf315575b98e651dc
Diogo <diogomf@hey.com>
2023-01-09 16:44:58 -0600

    fix: add require to presets on ui package tailwind config

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: "class",
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    presets: ["./tailwind.preset.js"],
+    presets: [require("./tailwind.preset.js")],
     prefix: "cui-",
     plugins: [require("@tailwindcss/typography")],
     theme: {
