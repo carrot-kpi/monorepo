d97882c8db54068099593514a2724e02cc5832a1
Federico Luzzi <fedeluzzi00@gmail.com>
2023-01-12 12:18:20 +0000

    feat(ui): add icon handling to button component

--- packages/ui/src/button/index.tsx
+++ packages/ui/src/button/index.tsx
@@ -1,9 +1,15 @@
-import React, { ReactNode } from "react";
+import React, {
+    ElementType,
+    FunctionComponent,
+    ReactNode,
+    SVGProps,
+} from "react";
 import { cva } from "class-variance-authority";
+import { ReactComponent as Spinner } from "../assets/spinner.svg";
 
 const buttonStyles = cva(
     [
-        "cui-font-mono cui-rounded-xxl cui-border cui-cursor-pointer cui-uppercase",
+        "cui-transition-colors cui-group cui-flex cui-items-center cui-font-mono cui-rounded-xxl cui-border cui-cursor-pointer cui-uppercase",
     ],
     {
         variants: {
@@ -22,12 +28,37 @@ const buttonStyles = cva(
     }
 );
 
+const iconStyles = cva([], {
+    variants: {
+        // variant: {
+        //     primary:
+        //         "cui-fill-white dark:cui-fill-black group-hover:cui-fill-black dark:group-hover:cui-fill-orange",
+        //     secondary:
+        //         "cui-fill-black dark:cui-fill-white group-hover:cui-fill-white dark:group-hover:cui-fill-black",
+        // },
+        iconPlacement: {
+            left: "cui-mr-2",
+            right: "cui-ml-2",
+        },
+        size: {
+            big: ["cui-w-6 cui-h-6"],
+            small: ["cui-w-5 cui-h-5"],
+            xsmall: ["cui-w-4 cui-h-4"],
+        },
+        loading: {
+            true: ["cui-animate-spin"],
+        },
+    },
+});
+
 export interface CarrotButtonProps {
     onClick?: (event: React.MouseEvent) => void;
     href?: string;
     disabled?: boolean;
     loading?: boolean;
     className?: string;
+    icon?: FunctionComponent<SVGProps<SVGSVGElement>>;
+    iconPlacement?: "left" | "right";
     size?: "big" | "small" | "xsmall";
     variant?: "primary" | "secondary";
     children: ReactNode;
@@ -42,29 +73,40 @@ export const Button = ({
     loading,
     children,
     className,
+    icon: Icon,
+    iconPlacement = "left",
 }: CarrotButtonProps) => {
-    if (href) {
-        return (
-            <a
-                className={buttonStyles({
+    const sharedProps = {
+        className: buttonStyles({
+            size,
+            variant,
+            className: [className, "cui-inline-block"],
+        }),
+    };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const [Root, props]: [ElementType, any] = !!href
+        ? ["a", { href }]
+        : ["button", { onClick, disabled: disabled || loading }];
+
+    let resolvedIcon;
+    if (!!loading)
+        resolvedIcon = (
+            <Spinner
+                className={iconStyles({
+                    iconPlacement,
                     size,
-                    variant,
-                    className: [className, "cui-inline-block"],
+                    loading,
                 })}
-                href={href}
-            >
-                {children}
-            </a>
+            />
         );
-    }
+    else if (!!Icon)
+        resolvedIcon = <Icon className={iconStyles({ iconPlacement, size })} />;
 
     return (
-        <button
-            disabled={disabled || loading}
-            className={buttonStyles({ size, variant, className })}
-            onClick={onClick}
-        >
-            {children}
-        </button>
+        <Root {...sharedProps} {...props}>
+            {iconPlacement === "left" && resolvedIcon}
+            <div>{children}</div>
+            {iconPlacement === "right" && resolvedIcon}
+        </Root>
     );
 };
