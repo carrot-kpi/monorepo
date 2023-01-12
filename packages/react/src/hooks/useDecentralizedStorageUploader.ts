543095071f79f2a822d5424a71e66f5058a61720
Federico Luzzi <fedeluzzi00@gmail.com>
2023-01-12 09:56:47 +0000

    feat(react): allow any content to be uploaded through the decentralized uploader

--- packages/react/src/hooks/useDecentralizedStorageUploader.ts
+++ packages/react/src/hooks/useDecentralizedStorageUploader.ts
@@ -2,24 +2,17 @@ import { KpiTokenSpecification } from "@carrot-kpi/sdk";
 
 export type UploadableSpecification = Omit<KpiTokenSpecification, "ipfsHash">;
 export type DecentralizedStorageOption = "ipfs" | "playground";
-export type SpecificationUploader = (
-    specification: UploadableSpecification
-) => Promise<string>;
+export type Uploader = (content: string) => Promise<string>;
 
 const WEB3_STORAGE_API_TOKEN =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNBNzU3RjVlNzUyRUE1M2IwYTE1QzA3MDJhODcyMTFjOUJGQTM5N0QiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDE4NDI5NTIzOTEsIm5hbWUiOiJqb2x0LXByZXZpZXctYnVpbGRzIn0.GMPv4WC8q-CIyCRjkIgG2EV2DnmD10bXd7sZoMcEVrs";
 
-const SPECIFICATION_UPLOADER: Record<
-    DecentralizedStorageOption,
-    SpecificationUploader
-> = {
-    ipfs: async (specification: UploadableSpecification): Promise<string> => {
+const UPLOADER: Record<DecentralizedStorageOption, Uploader> = {
+    ipfs: async (content: string): Promise<string> => {
         const formData = new FormData();
         formData.append(
             "file",
-            new File([JSON.stringify(specification)], "specification", {
-                type: "text/plain",
-            })
+            new File([content], "carrot-content", { type: "text/plain" })
         );
         const response = await fetch("https://api.web3.storage/upload", {
             method: "POST",
@@ -32,20 +25,16 @@ const SPECIFICATION_UPLOADER: Record<
         });
         if (!response.ok)
             throw new Error(
-                `could not upload file to ipfs, request failed with status ${response.status}`
+                `could not upload content to ipfs, request failed with status ${response.status}`
             );
         const { cid } = (await response.json()) as { cid: string };
         return cid;
     },
-    playground: async (
-        specification: UploadableSpecification
-    ): Promise<string> => {
+    playground: async (content: string): Promise<string> => {
         const formData = new FormData();
         formData.append(
             "file",
-            new File([JSON.stringify(specification)], "specification", {
-                type: "text/plain",
-            })
+            new File([content], "carrot-content", { type: "text/plain" })
         );
         const response = await fetch("http://localhost:5002/api/v0/add", {
             method: "POST",
@@ -56,7 +45,7 @@ const SPECIFICATION_UPLOADER: Record<
         });
         if (!response.ok)
             throw new Error(
-                `could not upload file to ipfs, request failed with status ${response.status}`
+                `could not upload content to ipfs, request failed with status ${response.status}`
             );
         const { Hash } = (await response.json()) as { Hash: string };
         return Hash;
@@ -65,6 +54,6 @@ const SPECIFICATION_UPLOADER: Record<
 
 export const useDecentralizedStorageUploader = (
     decentralizedStorage: DecentralizedStorageOption
-): SpecificationUploader => {
-    return SPECIFICATION_UPLOADER[decentralizedStorage];
+): Uploader => {
+    return UPLOADER[decentralizedStorage];
 };
