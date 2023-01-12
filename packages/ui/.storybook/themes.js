d5adc4ef0e05859156028157f38fe79c5cc3bcd2
Federico Luzzi <fedeluzzi00@gmail.com>
2023-01-12 09:57:51 +0000

    fix(ui): fix brand url in storybook config

--- packages/ui/.storybook/themes.js
+++ packages/ui/.storybook/themes.js
@@ -4,7 +4,7 @@ const createCustomThemeVariant = (base) => {
     return create({
         base,
         brandTitle: "Carrot UI",
-        brandUrl: "https://carrot.eth.limocom",
+        brandUrl: "https://carrot.eth.limo",
         brandImage: "/brand-image.svg",
         brandTarget: "_self",
         colorPrimary: "#EF692B",
