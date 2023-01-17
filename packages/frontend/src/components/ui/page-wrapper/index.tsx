0092b116df0fec8bfbf2625897d74d1630b9b471
Diogo <diogomf@hey.com>
2023-01-17 15:43:50 -0600

    fix: add cva for passing classNames styles on PageWrapper

--- packages/frontend/src/components/ui/page-wrapper/index.tsx
+++ packages/frontend/src/components/ui/page-wrapper/index.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode } from "react";
-
+import { cva } from "class-variance-authority";
 interface PageWrapperProps {
     children: ReactNode;
     className?: string;
 }
 
+const pageWrapperStyles = cva(["px-6", "lg:px-32"], {
+    variants: {},
+});
+
 export const PageWrapper = ({ children, className }: PageWrapperProps) => (
-    <div className={`px-6 lg:px-32 ${className ? className : ""}`}>
-        {children}
-    </div>
+    <div className={pageWrapperStyles({ className })}>{children}</div>
 );
