d4242333cc2cc76ba2214a646176142f2a732bf6
Paolo Guerra <pg.paologuerra@gmail.com>
2023-01-17 17:09:10 +0100

    fix(ui): remove unused import

--- packages/ui/src/input/markdown/index.tsx
+++ packages/ui/src/input/markdown/index.tsx
@@ -1,7 +1,7 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
-import React, { ReactNode, ReactElement, useCallback, useState } from "react";
+import React, { ReactElement, useCallback, useState } from "react";
 
 import { MenuBar } from "./menu-bar";
 import { TextMono } from "../../text-mono";
