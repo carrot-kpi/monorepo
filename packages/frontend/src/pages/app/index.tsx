c943b3a2613b2d20bf456fd11daf9bdca8e5af12
Federico Luzzi <fedeluzzi00@gmail.com>
2023-01-24 18:12:59 +0000

    feat(frontend): avoid passing ipfs gateway url to service worker

--- packages/frontend/src/pages/app/index.tsx
+++ packages/frontend/src/pages/app/index.tsx
@@ -5,21 +5,9 @@ import { Campaign } from "../campaign";
 import { Create } from "../create";
 import { Navbar } from "../../components/ui/navbar";
 import { Footer } from "../../components/ui/footer";
-import { IpfsService } from "@carrot-kpi/sdk";
 import { navbarLinks } from "./navbar-links";
 import { footerLinks } from "./footer-links";
 import { CarrotMarquee } from "../../components/ui/carrot-marquee";
-
-if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register(`/sw.js?ipfsGateway=${IpfsService.gateway}`)
-        .then(() => {
-            console.log("carrot service worker registered successfully");
-        })
-        .catch((error) => {
-            console.error("could not register carrot service worker", error);
-        });
-}
 
 export function App() {
     return (
