546d8b51796bc875a5665bca0acbe6194187d3a8
Federico Luzzi <fedeluzzi00@gmail.com>
2023-01-24 11:09:47 +0000

    feat(ui): implement remote logo component

---
+++ packages/ui/src/remote-logo/index.tsx
@@ -0,0 +1,79 @@
+import { cva, cx } from "class-variance-authority";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { resolveSrc } from "../utils/url";
+
+const rootStyles = cva(
+    ["cui-rounded-full", "cui-flex", "cui-justify-center", "cui-items-center"],
+    {
+        variants: {
+            size: {
+                md: ["cui-text-xxs", "cui-w-7", "cui-h-7"],
+                lg: ["cui-text-xs", "cui-w-8", "cui-h-8"],
+                xl: ["cui-text-sm", "cui-w-9", "cui-h-9"],
+                xxl: ["cui-text-md", "cui-w-10", "cui-h-10"],
+            },
+        },
+    }
+);
+
+export interface RemoteLogoProps {
+    src?: string | string[] | null;
+    defaultSrcs?: string | string[] | null;
+    size?: "md" | "lg" | "xl" | "xxl";
+    defaultText?: string | null;
+    ipfsGatewayURL?: string | null;
+    className?: { root: string };
+}
+
+export const RemoteLogo = ({
+    src,
+    defaultSrcs,
+    size = "md",
+    defaultText = "?",
+    ipfsGatewayURL,
+    className,
+}: RemoteLogoProps) => {
+    const resolvedSrcs = useMemo(() => {
+        return resolveSrc(src, ipfsGatewayURL, defaultSrcs);
+    }, [defaultSrcs, ipfsGatewayURL, src]);
+    const [resolvedDefaultText, setResolvedDefaultText] = useState("");
+    const [srcIndex, setSrcIndex] = useState(0);
+
+    useEffect(() => {
+        setResolvedDefaultText(
+            !!defaultText
+                ? defaultText.length > 4
+                    ? `${defaultText.slice(0, 3).toUpperCase()}`
+                    : defaultText.toUpperCase()
+                : "?"
+        );
+    }, [defaultText]);
+
+    const handleError = useCallback(() => {
+        setSrcIndex(srcIndex + 1);
+    }, [srcIndex]);
+
+    if (!!resolvedSrcs[srcIndex]) {
+        return (
+            <img
+                className={rootStyles({ size, className: className?.root })}
+                src={resolvedSrcs[srcIndex]}
+                onError={handleError}
+            />
+        );
+    }
+    return (
+        <div
+            className={cx(
+                rootStyles({ size }),
+                "cui-bg-black",
+                "dark:cui-bg-white",
+                className?.root
+            )}
+        >
+            <div className="cui-font-mono cui-text-white dark:cui-text-black">
+                {resolvedDefaultText}
+            </div>
+        </div>
+    );
+};
