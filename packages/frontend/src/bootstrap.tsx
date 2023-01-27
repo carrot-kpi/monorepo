4ff0af72a77897373f29199ef74e74387e6db227
Federico Luzzi <fedeluzzi00@gmail.com>
2023-01-27 09:22:24 +0000

    feat(frontend): import inter font with 700 weight

--- packages/frontend/src/bootstrap.tsx
+++ packages/frontend/src/bootstrap.tsx
@@ -18,6 +18,7 @@ import "./i18n";
 
 import "@fontsource/ibm-plex-mono/400.css";
 import "@fontsource/ibm-plex-mono/500.css";
+import "@fontsource/ibm-plex-mono/700.css";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/700.css";
