c1b856afcef385d83957b1d16e08d48e0bc5171d
Paolo Guerra <pg.paologuerra@gmail.com>
2023-01-30 19:05:58 +0100

    feat(ui): make chip component customizable

---
+++ packages/ui/src/data-display/chip/index.tsx
@@ -0,0 +1,26 @@
+import { cva } from "class-variance-authority";
+import React, { ReactNode } from "react";
+import { Typography } from "../typography";
+
+const rootStyles = cva([
+    "cui-bg-transparent",
+    "cui-cursor-default",
+    "cui-p-1",
+    "cui-px-2",
+    "cui-text-xs",
+    "cui-rounded-lg",
+    "cui-border",
+    "cui-border-black",
+    "dark:cui-border-white",
+]);
+
+export interface ChipProps {
+    children: ReactNode;
+    className?: { root?: string };
+}
+
+export const Chip = ({ children, className }: ChipProps) => (
+    <div className={rootStyles({ className: className?.root })}>
+        <Typography variant="sm">{children}</Typography>
+    </div>
+);
