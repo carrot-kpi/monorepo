f4cb3711facce6efdad1cede84ebba0171135957
Federico Luzzi <fedeluzzi00@gmail.com>
2023-02-06 13:35:38 +0000

    feat(frontend): avoid running a ipfs node in service worker

--- packages/frontend/src/react-app-env.d.ts
+++ packages/frontend/src/react-app-env.d.ts
@@ -3,14 +3,9 @@ import "react-i18next";
 
 import { resources } from "./i18n/resources";
 import { CARROT_KPI_FRONTEND_I18N_NAMESPACE } from "./constants";
-import { IPFS } from "ipfs-core";
 
 declare global {
     const __PREVIEW_MODE__: boolean;
-
-    interface WorkerGlobalScope {
-        ipfs: IPFS;
-    }
 }
 
 declare module "i18next" {
