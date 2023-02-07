054612602da595c3c14bf500047654e3fbe06d8c
Paolo Guerra <pg.paologuerra@gmail.com>
2023-02-07 12:25:00 +0100

    Implement Timer component (#95)
    * feat(ui): implement Timer component
    
    * feat(ui): support live countdown and reduce icon size in timer comp
    
    ---------
    
    Co-authored-by: Federico Luzzi <fedeluzzi00@gmail.com>

---
+++ packages/ui/src/utils/formatting/index.tsx
@@ -0,0 +1,24 @@
+export const DAY_MS = 86_400_000;
+export const HOUR_MS = 3_600_000;
+export const MINUTE_MS = 60_000;
+export const SECONDS_MS = 1_000;
+
+export const enforceDoubleDigits = (n: number) => {
+    return n < 10 ? `0${n}` : n;
+};
+
+export const formatCountDownString = (to: number) => {
+    const duration = to - Date.now();
+    if (duration <= 0) return "00D 00H 00M 00S";
+
+    const daysLeft = Math.floor(duration / DAY_MS);
+    const hoursLeft = Math.floor(duration / HOUR_MS) % 24;
+    const minutesLeft = Math.floor(duration / MINUTE_MS) % 60;
+    const secondsLeft = Math.floor(duration / SECONDS_MS) % 60;
+
+    return `${enforceDoubleDigits(daysLeft)}D ${enforceDoubleDigits(
+        hoursLeft
+    )}H ${enforceDoubleDigits(minutesLeft)}M ${enforceDoubleDigits(
+        secondsLeft
+    )}S`;
+};
