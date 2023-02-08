c917cdde4c915b39990ea062d766741e17caea29
Federico Luzzi <fedeluzzi00@gmail.com>
2023-02-08 15:16:14 +0000

    feat(frontend): implement use latest kpi tokens hook and use it

--- packages/frontend/src/pages/home/latest-campaigns-section/index.tsx
+++ packages/frontend/src/pages/home/latest-campaigns-section/index.tsx
@@ -1,33 +1,31 @@
-import { useKPITokens } from "@carrot-kpi/react";
 import { Button, Typography } from "@carrot-kpi/ui";
 import React from "react";
 import { Link } from "react-router-dom";
 import { CardHorizontal } from "../../../components/ui/cards-horizontal";
 import { KPITokenCard } from "../../../components/ui/kpi-token-card";
+import { useLatestKPITokens } from "../../../hooks/useLatestKPITokens";
+
+const LATEST_TOKEN_AMOUNTS = 5;
+
+const placeholder = new Array(LATEST_TOKEN_AMOUNTS)
+    .fill(null)
+    .map((_, index) => <KPITokenCard key={index} />);
 
 export const LatestCampaignsSection = () => {
-    const { loading, kpiTokens } = useKPITokens();
+    const { loading, kpiTokens } = useLatestKPITokens(LATEST_TOKEN_AMOUNTS);
 
     return (
         <div className="relative flex flex-col gap-16">
             <Typography variant="h2">Latest Campaigns</Typography>
             <CardHorizontal>
-                {loading ? (
-                    <>
-                        <KPITokenCard />
-                        <KPITokenCard />
-                        <KPITokenCard />
-                        <KPITokenCard />
-                        <KPITokenCard />
-                    </>
-                ) : (
-                    Object.values(kpiTokens).map((kpiToken) => (
-                        <KPITokenCard
-                            key={kpiToken.address}
-                            kpiToken={kpiToken}
-                        />
-                    ))
-                )}
+                {loading
+                    ? placeholder
+                    : Object.values(kpiTokens).map((kpiToken) => (
+                          <KPITokenCard
+                              key={kpiToken.address}
+                              kpiToken={kpiToken}
+                          />
+                      ))}
             </CardHorizontal>
             <Link to="/campaigns">
                 <Button>View all campaings</Button>
