8e55c378ba73d1c40b0f02a5c7ee893a71cec13f
Paolo Guerra <pg.paologuerra@gmail.com>
2023-02-08 18:41:21 +0100

    feat(frontend): use card component from the UI for the card

--- packages/frontend/src/components/ui/kpi-token-card/index.tsx
+++ packages/frontend/src/components/ui/kpi-token-card/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { Chip, Timer, Typography, Skeleton } from "@carrot-kpi/ui";
+import {
+    Chip,
+    Timer,
+    Typography,
+    Skeleton,
+    Card,
+    CardTitle,
+    CardContent,
+    CardActions,
+    Markdown,
+} from "@carrot-kpi/ui";
 import { cva } from "class-variance-authority";
+import sanitizeHtml from "sanitize-html";
 import { Link } from "react-router-dom";
 import { KPIToken } from "@carrot-kpi/sdk";
 
 const rootStyles = cva(
     [
-        "min-w-[320px] w-[320px] rounded-xxl flex flex-col justify-between border bg-white border-black dark:bg-black dark:border-white",
+        "min-w-[300px] max-w-[380px] rounded-xxl flex flex-col justify-between border bg-white border-black dark:bg-black dark:border-white",
     ],
     {
         variants: {
@@ -27,11 +38,19 @@ interface KPITokenCardProps {
 
 export const KPITokenCard = ({ kpiToken, noBorder }: KPITokenCardProps) => {
     return (
-        <div className={rootStyles({ noBorder })}>
-            <div className="h-60">
-                <div className="h-12 flex items-center w-full border-b border-gray-600 p-4">
-                    {/* TODO: handle logo fetching for creators */}
-                    {/* {!!kpiToken && (
+        <Card className={{ root: rootStyles({ noBorder }) }}>
+            <CardTitle>
+                <div className="flex items-center justify-between w-full">
+                    {!!kpiToken ? (
+                        <Typography weight="medium" uppercase>
+                            {kpiToken.specification.title}
+                        </Typography>
+                    ) : (
+                        <Skeleton width={80} />
+                    )}
+                </div>
+                {/* TODO: handle logo fetching for creators */}
+                {/* {!!kpiToken && (
                         <div className="flex items-center border-r border-gray-600">
                             <Skeleton
                                 className={{ root: "w-6 h-6 mx-3" }}
@@ -39,25 +58,21 @@ export const KPITokenCard = ({ kpiToken, noBorder }: KPITokenCardProps) => {
                             />
                         </div>
                     )} */}
-                    <div className="flex items-center justify-between w-full">
-                        {!!kpiToken ? (
-                            <Typography weight="medium" uppercase>
-                                {kpiToken.specification.title}
-                            </Typography>
-                        ) : (
-                            <Skeleton width={80} />
-                        )}
-                    </div>
-                </div>
-                <div className="h-48 flex flex-col justify-between p-4">
+            </CardTitle>
+            <CardContent>
+                <div className="h-40 flex flex-col justify-between p-4 overflow-hidden">
                     {!!kpiToken ? (
-                        <Typography
-                            className={{
-                                root: "line-clamp-5 overflow-hidden",
-                            }}
+                        <Markdown
+                            className={{ root: "prose-sm prose-headings:mt-0" }}
                         >
-                            {kpiToken.specification.description}
-                        </Typography>
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html: sanitizeHtml(
+                                        kpiToken.specification.description
+                                    ),
+                                }}
+                            />
+                        </Markdown>
                     ) : (
                         <div className="flex flex-col gap-3">
                             <Skeleton width="100%" />
@@ -66,57 +81,65 @@ export const KPITokenCard = ({ kpiToken, noBorder }: KPITokenCardProps) => {
                             <Skeleton width="20%" />
                         </div>
                     )}
-                    <div className="flex items-center space-x-3">
+                </div>
+                <div className="h-16 flex items-center gap-3 p-4">
+                    {!!kpiToken ? (
+                        <Chip>{kpiToken.template.specification.name}</Chip>
+                    ) : (
+                        <Skeleton variant="xl" width={80} />
+                    )}
+                    {!!kpiToken ? (
+                        kpiToken.specification.tags.map((tag) => (
+                            <Chip key={tag}>{tag}</Chip>
+                        ))
+                    ) : (
+                        <Skeleton variant="xl" width={60} />
+                    )}
+                </div>
+                <div className="h-12 flex items-center justify-between w-full border-t border-gray-600">
+                    <div className="flex items-center px-4 h-12 w-[40%]">
                         {!!kpiToken ? (
-                            <Chip>{kpiToken.template.specification.name}</Chip>
+                            <Typography uppercase>Time left</Typography>
                         ) : (
-                            <Skeleton variant="xl" width={80} />
+                            <Skeleton width="60%" />
                         )}
+                    </div>
+                    <div className="flex items-center justify-end px-4 h-12 w-[60%] border-l border-gray-600">
                         {!!kpiToken ? (
-                            kpiToken.specification.tags.map((tag) => (
-                                <Chip key={tag}>{tag}</Chip>
-                            ))
+                            kpiToken.expiration < Date.now() ? (
+                                <Typography
+                                    weight="medium"
+                                    uppercase
+                                    className={{ root: "text-red" }}
+                                >
+                                    Expired
+                                </Typography>
+                            ) : (
+                                <Timer
+                                    to={kpiToken.expiration}
+                                    countdown
+                                    icon
+                                />
+                            )
                         ) : (
-                            <Skeleton variant="xl" width={60} />
+                            <Skeleton width="60%" />
                         )}
                     </div>
                 </div>
-            </div>
-            <div className="h-12 flex items-center justify-between w-full border-t border-gray-600">
-                <div className="flex items-center px-4 h-12 w-[40%]">
-                    {!!kpiToken ? (
-                        <Typography uppercase>Time left</Typography>
-                    ) : (
-                        <Skeleton width="60%" />
-                    )}
-                </div>
-                <div className="flex items-center justify-end px-4 h-12 w-[60%] border-l border-gray-600">
-                    {!!kpiToken ? (
-                        kpiToken.expiration < Date.now() ? (
-                            <Typography
-                                weight="medium"
-                                uppercase
-                                className={{ root: "text-red" }}
-                            >
-                                Expired
+            </CardContent>
+            <CardActions className={{ root: "justify-center" }}>
+                <Link to={!!kpiToken ? `/campaigns/${kpiToken.address}` : ""}>
+                    <div className="flex justify-center items-center w-full">
+                        {!!kpiToken ? (
+                            <Typography weight="medium">
+                                ↳ VIEW CAMPAIGN
                             </Typography>
                         ) : (
-                            <Timer to={kpiToken.expiration} countdown icon />
-                        )
-                    ) : (
-                        <Skeleton width="60%" />
-                    )}
-                </div>
-            </div>
-            <Link to={!!kpiToken ? `/campaigns/${kpiToken.address}` : ""}>
-                <div className="h-14 flex justify-center items-center w-full border-t border-gray-600">
-                    {!!kpiToken ? (
-                        <Typography weight="medium">↳ VIEW CAMPAIGN</Typography>
-                    ) : (
-                        <Skeleton width="40%" />
-                    )}
-                </div>
-            </Link>
-        </div>
+                            <Skeleton width="40%" />
+                        )}
+                    </div>
+                </Link>
+            </CardActions>
+        </Card>
     );
 };
