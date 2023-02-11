3a8fa2e41147babdd47d4446de240c35bae98f91
Federico Luzzi <fedeluzzi00@gmail.com>
2023-02-11 12:55:40 +0000

    feat(frontend): rework campaigns page

---
+++ packages/frontend/src/utils/kpi-tokens.ts
@@ -0,0 +1,26 @@
+import { KPIToken } from "@carrot-kpi/sdk";
+
+export const filterKPITokens = (tokens: KPIToken[], activeOnly?: boolean) => {
+    if (tokens.length === 0) return [];
+    if (typeof activeOnly === "undefined") return tokens;
+    return tokens.filter((token) => {
+        return activeOnly ? !token.expired : token.expired;
+    });
+};
+
+export const sortKPITokens = (
+    tokens: KPIToken[],
+    ordering?: "newest-first" | "oldest-first"
+) => {
+    if (tokens.length === 0) return [];
+    const comparator =
+        ordering === "newest-first"
+            ? (a: KPIToken, b: KPIToken) => {
+                  return b.creationTimestamp - a.creationTimestamp;
+              }
+            : (a: KPIToken, b: KPIToken) => {
+                  return a.creationTimestamp - b.creationTimestamp;
+              };
+
+    return tokens.sort(comparator);
+};
