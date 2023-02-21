8afd92a30746c66b28a993170fb1148168c95d9d
Paolo Guerra <pg.paologuerra@gmail.com>
2023-02-21 17:48:54 +0100

    Add stepper component (#125)
    * feat(ui): add stepper component
    
    * refactor: change stepper behaviour
    
    * feat(ui): add automatic layout for stepper
    
    ---------
    
    Co-authored-by: Federico Luzzi <fedeluzzi00@gmail.com>

--- packages/ui/tailwind.preset.js
+++ packages/ui/tailwind.preset.js
@@ -71,5 +71,8 @@ module.exports = {
             },
         },
     },
-    plugins: [require("@tailwindcss/typography")],
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/line-clamp"),
+    ],
 };
