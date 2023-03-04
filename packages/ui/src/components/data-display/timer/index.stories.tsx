4c1330b1f852fc24e5aab575df34a0468bc6153e
Federico Luzzi <fedeluzzi00@gmail.com>
2023-03-04 23:03:33 +0000

    feat(ui): use dayjs in internal accounting of durations in timer

--- packages/ui/src/components/data-display/timer/index.stories.tsx
+++ packages/ui/src/components/data-display/timer/index.stories.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { ComponentMeta, Story } from "@storybook/react";
 
 import { Timer as TimerComponent, TimerProps } from ".";
-import { DAY_MS } from "../../../utils/formatting";
 
 export default {
     title: "Data display/Timer",
@@ -15,5 +14,5 @@ const Template: Story<TimerProps> = (props: TimerProps) => {
 
 export const Timer: Story<TimerProps> = Template.bind({});
 Timer.args = {
-    to: Date.now() + DAY_MS * 2,
+    to: Math.floor(Date.now() + (86_400 * 2) / 1000),
 };
--- packages/ui/src/components/data-display/timer/index.tsx
+++ packages/ui/src/components/data-display/timer/index.tsx
@@ -1,22 +1,21 @@
 import React, { ReactElement, useEffect, useState } from "react";
-import { formatCountDownString } from "../../../utils/formatting";
-import { Typography } from "../typography";
+import { Typography, TypographyProps } from "../typography";
 import { ReactComponent as ClockSvg } from "../../../assets/clock.svg";
 import { mergedCva } from "../../../utils/components";
+import dayjs from "dayjs";
+import Duration from "dayjs/plugin/duration";
+
+dayjs.extend(Duration);
 
 const rootStyles = mergedCva(["cui-flex", "cui-gap-2", "cui-items-center"]);
 
-const iconStyles = mergedCva([
-    "cui-h-4",
-    "cui-w-4",
-    "cui-stroke-black",
-    "dark:cui-stroke-white",
-]);
+const iconStyles = mergedCva(["cui-text-black", "dark:cui-text-white"]);
 
 export interface TimerProps {
     to: number;
     icon?: boolean;
     countdown?: boolean;
+    variant?: TypographyProps["variant"];
     className?: {
         root?: string;
         icon?: string;
@@ -26,31 +25,35 @@ export interface TimerProps {
 export const Timer = ({
     to,
     countdown,
+    variant,
     icon,
     className,
 }: TimerProps): ReactElement => {
-    const [countdownString, setCountdownString] = useState(
-        formatCountDownString(to)
+    const [duration, setDuration] = useState(
+        dayjs.duration(dayjs.unix(to).diff(dayjs()))
     );
 
     useEffect(() => {
         if (!countdown) return;
         const timer = setInterval(() => {
-            setCountdownString(formatCountDownString(to));
+            setDuration(dayjs.duration(dayjs.unix(to).diff(dayjs())));
         }, 1_000);
         return () => {
             clearInterval(timer);
         };
-    }, [countdown, to]);
+    }, [countdown, duration, to]);
 
     return (
-        <div className={rootStyles({ className: className?.root })}>
+        <Typography
+            variant={variant}
+            className={{ root: rootStyles({ className: className?.root }) }}
+        >
             {icon && (
                 <ClockSvg
                     className={iconStyles({ className: className?.icon })}
                 />
             )}
-            <Typography variant="sm">{countdownString}</Typography>
-        </div>
+            {duration.format("DD[D] HH[H] mm[M] ss[S]")}
+        </Typography>
     );
 };
