3939f78c22f5e7df5725dd56bb2d1cd9356c47b7
Federico Luzzi <fedeluzzi00@gmail.com>
2023-03-07 21:20:57 +0000

    feat: new home hero when no featured campaigns are present

---
+++ packages/frontend/src/hooks/useFeaturedKPITokens.ts
@@ -0,0 +1,17 @@
+import { KPIToken } from "@carrot-kpi/sdk";
+import { useQuery } from "@tanstack/react-query";
+
+export function useFeaturedKPITokens(): {
+    loading: boolean;
+    kpiTokens: KPIToken[];
+} {
+    const { data: kpiTokens, isLoading: loading } = useQuery({
+        queryKey: ["featueredKPITokens"],
+        queryFn: async () => {
+            // TODO: implement
+            return [];
+        },
+    });
+
+    return { loading: loading, kpiTokens: kpiTokens || [] };
+}
--- packages/frontend/src/pages/home/hero/index.tsx
+++ packages/frontend/src/pages/home/hero/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from "@carrot-kpi/ui";
+import { Button, Loader, Typography } from "@carrot-kpi/ui";
 import React from "react";
 import { FeaturedCampaigns } from "../../../components/featured-campaigns";
 import { CardHorizontal } from "../../../components/ui/cards-horizontal";
@@ -8,6 +8,7 @@ import { ReactComponent as Plus } from "../../../assets/plus.svg";
 import { useTranslation } from "react-i18next";
 import { cva } from "class-variance-authority";
 import { CreateCampaignButton } from "../../../components/create-campaign-button";
+import { useFeaturedKPITokens } from "../../../hooks/useFeaturedKPITokens";
 
 const plusIconStyles = cva(["invisible", "md:visible", "absolute"], {
     variants: {
@@ -25,28 +26,53 @@ const plusIconStyles = cva(["invisible", "md:visible", "absolute"], {
 
 export const Hero = () => {
     const { t } = useTranslation();
+    const { loading, kpiTokens } = useFeaturedKPITokens();
 
     return (
-        <div className="relative bg-orange bg-grid-light">
-            <div className="relative space-y-12 pt-7 pb-16 md:pt-24 md:pb-32">
-                <Typography
-                    variant="h1"
-                    className={{
-                        root: "px-6 md:px-10 lg:px-32 dark:text-black",
-                    }}
-                >
-                    {t("home.featuredCampaigns")}
-                </Typography>
-                <CardHorizontal className="px-6 md:px-10 lg:px-32 dark">
-                    <FeaturedCampaigns />
-                </CardHorizontal>
-                <div className="px-6 md:px-10 lg:px-32 flex flex-col space-x-0 md:space-x-8 space-y-4 md:space-y-0 md:flex-row">
-                    <Button variant="primary" size="big">
-                        <Link to="/campaigns">{t("campaign.all")}</Link>
-                    </Button>
-                    {!__PREVIEW_MODE__ && <CreateCampaignButton />}
+        <div className="relative bg-orange bg-grid-light min-h-[65vh]">
+            {loading ? (
+                <div className="flex justify-center items-center w-full h-full">
+                    <Loader />
                 </div>
-            </div>
+            ) : kpiTokens.length === 0 ? (
+                <div className="px-6 md:px-14 lg:px-36 relative space-y-12 pt-7 pb-16 md:pt-24 md:pb-32">
+                    <div className="flex flex-col md:flex-row justify-around items-center gap-10 md:gap-0">
+                        <div className="flex flex-col items-center md:items-start gap-10 w-full md:w-2/5">
+                            <Typography variant="h1">
+                                Reach your goals with a Carrot
+                            </Typography>
+                            <Typography>
+                                Easy and powerful tool to create conditional
+                                tokens allowing an effective method to achieve
+                                any goal desirable using permissionless,
+                                decentralized technologies.
+                            </Typography>
+                            <Button>Create campaign</Button>
+                        </div>
+                        <div className="w-full md:w-1/2 aspect-video bg-gray-600 rounded-xl"></div>
+                    </div>
+                </div>
+            ) : (
+                <div className="relative space-y-12 pt-7 pb-16 md:pt-24 md:pb-32">
+                    <Typography
+                        variant="h1"
+                        className={{
+                            root: "px-6 md:px-10 lg:px-32 dark:text-black",
+                        }}
+                    >
+                        {t("home.featuredCampaigns")}
+                    </Typography>
+                    <CardHorizontal className="px-6 md:px-10 lg:px-32 dark">
+                        <FeaturedCampaigns />
+                    </CardHorizontal>
+                    <div className="px-6 md:px-10 lg:px-32 flex flex-col space-x-0 md:space-x-8 space-y-4 md:space-y-0 md:flex-row">
+                        <Button variant="primary" size="big">
+                            <Link to="/campaigns">{t("campaign.all")}</Link>
+                        </Button>
+                        {!__PREVIEW_MODE__ && <CreateCampaignButton />}
+                    </div>
+                </div>
+            )}
             <div className="absolute invisible left-4 top-1/3 lg:visible ">
                 <DXdaoSideLink />
             </div>
