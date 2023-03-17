9caf243a052c84326f23bfb5a2c9112218b3b78c
Federico Luzzi <fedeluzzi00@gmail.com>
2023-03-17 16:19:49 +0000

    feat(ui): make erc20 token logo and remote logo in general snappier (#180)

--- packages/ui/src/components/data-display/erc20-token-logo/index.tsx
+++ packages/ui/src/components/data-display/erc20-token-logo/index.tsx
@@ -5,16 +5,19 @@ import { getDefaultERC20TokenLogoURL } from "../../../utils/erc20";
 export type ERC20TokenLogoProps = {
     chainId: number;
     address: string;
+    symbol?: string;
 } & RemoteLogoProps;
 
 export const ERC20TokenLogo = ({
     chainId,
     address,
+    symbol,
     ...rest
 }: ERC20TokenLogoProps) => {
     return (
         <RemoteLogo
             defaultSrc={getDefaultERC20TokenLogoURL(chainId, address)}
+            defaultText={symbol}
             {...rest}
         />
     );
