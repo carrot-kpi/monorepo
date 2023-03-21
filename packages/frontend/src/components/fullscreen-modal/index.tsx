a3c43c80614a6c13b0d5a627d74f4c3cff57062f
Paolo Guerra <pg.paologuerra@gmail.com>
2023-03-21 21:43:34 +0100

    fix(frontend): remove horizontal scrolling from fullscreen modal (#192)

--- packages/frontend/src/components/fullscreen-modal/index.tsx
+++ packages/frontend/src/components/fullscreen-modal/index.tsx
@@ -4,7 +4,15 @@ import { animated, SpringValue } from "@react-spring/web";
 import { cva } from "class-variance-authority";
 
 const rootStyles = cva(
-    ["fixed", "top-0", "left-0", "h-screen", "w-screen", "overflow-y-auto"],
+    [
+        "fixed",
+        "top-0",
+        "left-0",
+        "h-screen",
+        "w-screen",
+        "overflow-y-auto",
+        "overflow-x-hidden",
+    ],
     {
         variants: {
             bgColor: {
