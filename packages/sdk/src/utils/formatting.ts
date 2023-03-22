30a5089b77569b8d68da90c0967770e6f1116b40
Federico Luzzi <fedeluzzi00@gmail.com>
2023-03-22 12:06:21 +0000

    feat(sdk): add decimals formatting utils

---
+++ packages/sdk/src/utils/formatting.ts
@@ -0,0 +1,31 @@
+import { commify, formatUnits } from "@ethersproject/units";
+import { Amount } from "../entities/amount";
+import { Token } from "../entities/token";
+
+export const formatTokenAmount = (
+    amount: Amount<Token>,
+    withSymbol = true,
+    nonZeroDecimalsAmount = 4
+) => {
+    let rawBaseAmount = formatDecimals(
+        commify(formatUnits(amount.raw, amount.currency.decimals)),
+        nonZeroDecimalsAmount
+    );
+    if (withSymbol)
+        rawBaseAmount = rawBaseAmount + ` ${amount.currency.symbol}`;
+    return rawBaseAmount;
+};
+
+export const formatDecimals = (number: string, decimalsAmount = 4) => {
+    const decimalIndex = number.indexOf(".");
+    if (decimalIndex === -1) return number;
+    let i = decimalIndex + 1;
+    while (i < number.length) {
+        if (number[i] !== "0") {
+            i += decimalsAmount;
+            break;
+        }
+        i++;
+    }
+    return number.substring(0, i);
+};
