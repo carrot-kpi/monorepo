d020a62cfff66a6a46cf5ac5a538ecf81632416f
Paolo Guerra <pg.paologuerra@gmail.com>
2023-03-27 15:25:16 +0200

    refactor(ui): use error svg

--- packages/ui/src/components/feedback/error-feedback/index.tsx
+++ packages/ui/src/components/feedback/error-feedback/index.tsx
@@ -20,7 +20,6 @@ export const ErrorFeedback = ({
     className,
 }: ErrorFeedbackProps): ReactElement => {
     return (
-        // TODO: refactor this component once we have the desings
         <div className="cui-flex cui-flex-col cui-gap-4 cui-items-center cui-p-8 cui-max-w-lg cui-rounded-xl cui-border cui-border-black dark:cui-border-white cui-bg-white dark:cui-bg-black">
             <ErrorSvg
                 className={cx(
@@ -28,10 +27,14 @@ export const ErrorFeedback = ({
                     className?.icon
                 )}
             />
-            <Typography variant="h4" className={className?.title}>
+            <Typography
+                variant="2xl"
+                weight="bold"
+                className={className?.title}
+            >
                 {messages.title}
             </Typography>
-            <Typography variant="lg" className={className?.description}>
+            <Typography className={className?.description}>
                 {messages.description}
             </Typography>
         </div>
