1ffca39412090d105553285e1f1013bd1ff47e7a
Federico Luzzi <fedeluzzi00@gmail.com>
2023-04-12 21:19:19 +0200

    feat(ui): typography improvements (#231)
    * chore(ui): v0.38.0
    
    * feat(ui): improve typography settings to have good balance between documentation and frontend
    
    * feat(ui): improved margin top in headings
    
    * fix: comment typo

--- packages/ui/src/components/data-display/markdown/index.stories.tsx
+++ packages/ui/src/components/data-display/markdown/index.stories.tsx
@@ -14,6 +14,58 @@ const Template: Story<MarkdownProps> = (props: MarkdownProps) => (
 
 export const Markdown: Story<MarkdownProps> = Template.bind({});
 Markdown.args = {
-    children:
-        "<h3>Uniswap campaign</h3><p>will <em>uniswap v3</em> volume reach <em>34b</em> in 30 days?</p><pre><code>some code</code></pre>",
+    children: (
+        <>
+            <h1>H1 heading</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <h2>H2 heading</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <h3>H3 heading</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <h4>H4 heading</h4>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <h5>H5 heading</h5>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <h6>H6 heading</h6>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <pre>
+                <code>some code</code>
+            </pre>
+            <ul>
+                <li>List element 1</li>
+                <li>List element 2</li>
+                <li>List element 3</li>
+            </ul>
+        </>
+    ),
 };
