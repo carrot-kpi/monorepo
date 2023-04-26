557ee99447d9c9560a6d0519ecd7e79823758906
Federico Luzzi <fedeluzzi00@gmail.com>
2023-04-26 13:53:11 +0100

    fix: fix custom eslint config lib

--- packages/ui/src/icons/clock.tsx
+++ packages/ui/src/icons/clock.tsx
@@ -15,11 +15,7 @@ const Clock = (props: SVGIcon) => (
                 d="M22.5 12c0 5.799-4.701 10.5-10.5 10.5S1.5 17.799 1.5 12 6.201 1.5 12 1.5 22.5 6.201 22.5 12Z"
                 stroke="currentColor"
             />
-            <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                d="M12 5v7l4 4"
-            />
+            <path stroke="currentColor" strokeLinecap="round" d="M12 5v7l4 4" />
         </g>
     </svg>
 );
