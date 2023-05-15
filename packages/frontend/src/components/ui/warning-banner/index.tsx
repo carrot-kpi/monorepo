069c2eb3e8e9650abb0d943378ab5c807cdbfc68
Paolo Guerra <pg.paologuerra@gmail.com>
2023-05-15 14:40:24 +0200

    feat(frontend): add warning message when staging mode is enabled

---
+++ packages/frontend/src/components/ui/warning-banner/index.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from "react";
+
+interface WarningBannerProps {
+    children: ReactNode;
+}
+
+export const WarningBanner = ({ children }: WarningBannerProps) => {
+    return <div className="bg-yellow px-10 py-6">{children}</div>;
+};
