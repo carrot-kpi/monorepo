143143c5ee43e1550d3cb652281b0556c5957dfc
Paolo Guerra <pg.paologuerra@gmail.com>
2023-05-15 22:19:26 +0200

    refactor(frontend): use Marquee from UI

--- packages/frontend/src/components/ui/carrot-marquee/index.tsx
+++ packages/frontend/src/components/ui/carrot-marquee/index.tsx
@@ -1,11 +1,11 @@
-import React, { ReactNode } from "react";
+import { Marquee } from "@carrot-kpi/ui";
+import React from "react";
 import CommunityDrivenLogo from "../../../icons/marquee/community-driven";
 import NetworkLogo from "../../../icons/marquee/network";
 import Decentralized100Logo from "../../../icons/marquee/decentralized100";
 import StarsLogo from "../../../icons/marquee/stars";
 import CommunityLogo from "../../../icons/marquee/community";
 import ClaimBounties from "../../../icons/marquee/claim-bounties";
-import { cva } from "class-variance-authority";
 
 const GreenLogos = () => (
     <>
@@ -23,43 +23,10 @@ const YellowLogos = () => (
     </>
 );
 
-interface MarqueeProps {
-    children: ReactNode;
-    color: "green" | "yellow";
-}
-
-const marqueeMainStyles = cva(["flex overflow-x-hidden"], {
-    variants: {
-        color: {
-            green: ["bg-green"],
-            yellow: ["bg-yellow"],
-        },
-    },
-});
-
-const marqueeRowStyles = cva(
-    ["relative flex items-center h-12 space-x-6 md:h-16"],
-    {
-        variants: {
-            color: {
-                green: ["animate-marquee-fast"],
-                yellow: ["animate-marquee-slow"],
-            },
-        },
-    }
-);
-
-const MarqueeRow = ({ children, color }: MarqueeProps) => (
-    <div className={marqueeMainStyles({ color })}>
-        <div className={marqueeRowStyles({ color })}>{children}</div>
-        <div className={marqueeRowStyles({ color })}>{children}</div>
-    </div>
-);
-
 export const CarrotMarquee = () => {
     return (
         <div>
-            <MarqueeRow color="green">
+            <Marquee color="green">
                 <GreenLogos />
                 <GreenLogos />
                 <GreenLogos />
@@ -68,8 +35,10 @@ export const CarrotMarquee = () => {
                 <GreenLogos />
                 <GreenLogos />
                 <GreenLogos />
-            </MarqueeRow>
-            <MarqueeRow color="yellow">
+                <GreenLogos />
+                <GreenLogos />
+            </Marquee>
+            <Marquee color="yellow">
                 <YellowLogos />
                 <YellowLogos />
                 <YellowLogos />
@@ -78,7 +47,9 @@ export const CarrotMarquee = () => {
                 <YellowLogos />
                 <YellowLogos />
                 <YellowLogos />
-            </MarqueeRow>
+                <YellowLogos />
+                <YellowLogos />
+            </Marquee>
         </div>
     );
 };
