ebb5b134ec9db177881c7dbfcf5de6a65f174d47
Federico Luzzi <fedeluzzi00@gmail.com>
2023-06-02 11:41:33 +0100

    feat(frontend): reduce carrot logo width in footer

--- packages/frontend/src/components/ui/footer/index.tsx
+++ packages/frontend/src/components/ui/footer/index.tsx
@@ -10,7 +10,7 @@ interface FooterProps {
 export const Footer = ({ footerLinks }: FooterProps) => (
     <div className="w-full py-16 bg-black md:py-24 lg:py-32">
         <div className="relative h-full mx-auto w-full md:w-[96%] bg-grid-dark">
-            <div className="mx-auto py-14 md:py-24 xl:py-32 w-[90%]">
+            <div className="mx-auto py-14 md:py-24 xl:py-32 w-[80%]">
                 <Logo className="relative w-full h-auto text-orange" />
             </div>
         </div>
