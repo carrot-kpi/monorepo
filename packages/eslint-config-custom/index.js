310f2d880c25a0277d0cf8ac76c29c0d87a2042a
Federico Luzzi <federico.luzzi@proton.me>
2023-07-26 08:54:24 +0100

    fix: changeset (#312)
    * chore(eslint-config-custom): make package private
    
    * chore: ignore changelog in prettier and eslint
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: Federico Luzzi <fedeluzzi00@gmail.com>

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,7 +2,13 @@ module.exports = {
     extends: ["turbo", "prettier", "plugin:@typescript-eslint/recommended"],
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint", "prettier"],
-    ignorePatterns: ["node_modules/", "dist/", "build/", ".turbo/"],
+    ignorePatterns: [
+        "node_modules/",
+        "dist/",
+        "build/",
+        ".turbo/",
+        "CHANGELOG.md",
+    ],
     rules: {
         "prettier/prettier": "error",
     },
