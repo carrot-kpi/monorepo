1917d9bc2e4b68b86dc9b0987033e87fbca233eb
Paolo Guerra <pg.paologuerra@gmail.com>
2023-07-26 17:55:44 +0200

    feat(frontend): implement featured KPI tokens

--- packages/frontend/src/hooks/useFeaturedKPITokens.ts
+++ packages/frontend/src/hooks/useFeaturedKPITokens.ts
@@ -1,15 +1,45 @@
-import { KPIToken } from "@carrot-kpi/sdk";
+import { usePreferDecentralization } from "@carrot-kpi/react";
+import { ChainId, Fetcher, KPIToken, enforce } from "@carrot-kpi/sdk";
 import { useQuery } from "@tanstack/react-query";
+import { useNetwork, usePublicClient, type Address } from "wagmi";
+import { FEATURED_BLACKLISTED_KPI_TOKENS_CONFIGURATION_LOCATION } from "../constants";
+
+type FeaturedBlacklistedKPITokens = Record<
+    ChainId,
+    { featured: Address[]; blacklisted: Address[] }
+>;
+
+export const FEATURED_KPI_TOKEN_QUERY_KEY_PREFIX =
+    "featuredKPITokens" as string;
 
 export function useFeaturedKPITokens(): {
     loading: boolean;
     kpiTokens: KPIToken[];
 } {
+    const preferDecentralization = usePreferDecentralization();
+    const { chain } = useNetwork();
+    const publicClient = usePublicClient();
+
     const { data: kpiTokens, isLoading: loading } = useQuery({
-        queryKey: ["featueredKPITokens"],
+        queryKey: [FEATURED_KPI_TOKEN_QUERY_KEY_PREFIX, { chain }],
         queryFn: async () => {
-            // TODO: implement
-            return [];
+            if (!chain) return [];
+            enforce(chain.id in ChainId, "unsupported chain");
+
+            const featuredBlacklistedKPITokens = (await (
+                await fetch(
+                    FEATURED_BLACKLISTED_KPI_TOKENS_CONFIGURATION_LOCATION,
+                )
+            ).json()) as FeaturedBlacklistedKPITokens;
+
+            const kpiTokens = await Fetcher.fetchKPITokens({
+                publicClient,
+                preferDecentralization,
+                addresses:
+                    featuredBlacklistedKPITokens[chain.id as ChainId].featured,
+            });
+
+            return Object.values(kpiTokens).reverse();
         },
     });
 
