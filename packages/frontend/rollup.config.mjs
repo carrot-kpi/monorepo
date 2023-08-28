1cb77331bd31c983b4edc5e19bcdf1bebc6e4c39
Paolo Guerra <pg.paologuerra@gmail.com>
2023-08-28 21:32:23 +0200

    fix(frontend): add image rollup plugin

--- packages/frontend/rollup.config.mjs
+++ packages/frontend/rollup.config.mjs
@@ -4,6 +4,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
+import image from "@rollup/plugin-image";
 import postcss from "rollup-plugin-postcss";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
@@ -45,6 +46,7 @@ export default [
             }),
             json(),
             typescript(),
+            image(),
         ],
         output: {
             dir: resolve("dist"),
