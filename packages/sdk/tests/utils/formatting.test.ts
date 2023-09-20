3618512bd3d8a065e82fd8cb921274430f74a28d
Federico Luzzi <federico.luzzi@proton.me>
2023-09-20 14:24:14 +0100

    feat(sdk): add commify option in format decimals utility function (#389)
    * feat(sdk): add commify option in format decimals utility function
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: luzzifoss <fedeluzzi00@gmail.com>

---
+++ packages/sdk/tests/utils/formatting.test.ts
@@ -0,0 +1,223 @@
+import { describe, test } from "@jest/globals";
+import { parseUnits } from "viem";
+import { privateKeyToAccount, generatePrivateKey } from "viem/accounts";
+import { ChainId } from "../../src/commons";
+import { Token } from "../../src/entities/token";
+import { Amount } from "../../src/entities/amount";
+import { formatCurrencyAmount } from "../../src/utils";
+
+describe("formatting utilities", () => {
+    let token: Token;
+
+    beforeAll(() => {
+        token = new Token(
+            ChainId.SEPOLIA,
+            privateKeyToAccount(generatePrivateKey()).address,
+            18,
+            "TST1",
+            "Test token 1",
+        );
+    });
+
+    describe("format currency", () => {
+        describe("no symbol, no commify", () => {
+            test("works with integer numbers < 1000", () => {
+                const value = parseUnits("1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: false,
+                        commify: false,
+                    }),
+                ).toBe("1");
+            });
+
+            test("works with integer numbers >= 1000", () => {
+                const value = parseUnits("1000", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: false,
+                        commify: false,
+                    }),
+                ).toBe("1000");
+            });
+
+            test("works with decimal numbers < 1000", () => {
+                const value = parseUnits("1.1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: false,
+                        commify: false,
+                    }),
+                ).toBe("1.1");
+            });
+
+            test("works with decimal numbers >= 1000", () => {
+                const value = parseUnits("1000.1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: false,
+                        commify: false,
+                    }),
+                ).toBe("1000.1");
+            });
+        });
+
+        describe("with symbol, no commify", () => {
+            test("works with integer numbers < 1000", () => {
+                const value = parseUnits("1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: true,
+                        commify: false,
+                    }),
+                ).toBe(`1 ${token.symbol}`);
+            });
+
+            test("works with integer numbers >= 1000", () => {
+                const value = parseUnits("1000", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: true,
+                        commify: false,
+                    }),
+                ).toBe(`1000 ${token.symbol}`);
+            });
+
+            test("works with decimal numbers < 1000", () => {
+                const value = parseUnits("1.1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: true,
+                        commify: false,
+                    }),
+                ).toBe(`1.1 ${token.symbol}`);
+            });
+
+            test("works with decimal numbers >= 1000", () => {
+                const value = parseUnits("1000.1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: true,
+                        commify: false,
+                    }),
+                ).toBe(`1000.1 ${token.symbol}`);
+            });
+        });
+
+        describe("with symbol, with commify", () => {
+            test("works with integer numbers < 1000", () => {
+                const value = parseUnits("1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: true,
+                        commify: true,
+                    }),
+                ).toBe(`1 ${token.symbol}`);
+            });
+
+            test("works with integer numbers >= 1000", () => {
+                const value = parseUnits("1000", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: true,
+                        commify: true,
+                    }),
+                ).toBe(`1,000 ${token.symbol}`);
+            });
+
+            test("works with decimal numbers < 1000", () => {
+                const value = parseUnits("1.1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: true,
+                        commify: true,
+                    }),
+                ).toBe(`1.1 ${token.symbol}`);
+            });
+
+            test("works with decimal numbers >= 1000", () => {
+                const value = parseUnits("1000.1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: true,
+                        commify: true,
+                    }),
+                ).toBe(`1,000.1 ${token.symbol}`);
+            });
+        });
+
+        describe("no symbol, with commify", () => {
+            test("works with integer numbers < 1000", () => {
+                const value = parseUnits("1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: false,
+                        commify: true,
+                    }),
+                ).toBe("1");
+            });
+
+            test("works with integer numbers >= 1000", () => {
+                const value = parseUnits("1000", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: false,
+                        commify: true,
+                    }),
+                ).toBe("1,000");
+            });
+
+            test("works with decimal numbers < 1000", () => {
+                const value = parseUnits("1.1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: false,
+                        commify: true,
+                    }),
+                ).toBe("1.1");
+            });
+
+            test("works with decimal numbers >= 1000", () => {
+                const value = parseUnits("1000.1", token.decimals);
+                const amount = new Amount(token, value);
+                expect(
+                    formatCurrencyAmount({
+                        amount,
+                        withSymbol: false,
+                        commify: true,
+                    }),
+                ).toBe("1,000.1");
+            });
+        });
+    });
+});
