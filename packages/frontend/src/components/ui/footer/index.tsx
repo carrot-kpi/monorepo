13e231b540fbeb6dfb4313244d78f6e71a5bd862
luzzifoss <fedeluzzi00@gmail.com>
2023-10-11 18:00:32 +0100

    feat(frontend): enforce maximum width in footer section

--- packages/frontend/src/components/ui/footer/index.tsx
+++ packages/frontend/src/components/ui/footer/index.tsx
@@ -8,13 +8,13 @@ interface FooterProps {
 }
 
 export const Footer = ({ footerLinks }: FooterProps) => (
-    <div className="w-full py-16 bg-black md:py-24 lg:py-32">
-        <div className="relative h-full mx-auto w-full md:w-[96%] bg-grid-dark">
+    <div className="flex flex-col items-center w-full py-16 bg-black md:py-24 lg:py-32 px-6 md:px-24 xl:px-32">
+        <div className="relative h-full mx-auto w-full max-w-screen-2xl bg-grid-dark">
             <div className="mx-auto py-14 md:py-24 xl:py-32 w-[80%]">
                 <Logo className="relative w-full h-auto text-orange" />
             </div>
         </div>
-        <div className="flex flex-col justify-between px-6 pt-8 space-y-24 md:pt-24 lg:pt-32 md:px-24 xl:px-32 xl:space-y-0 xl:flex-row xxl:px-52">
+        <div className="w-full max-w-screen-2xl flex flex-col justify-between pt-8 space-y-24 md:pt-24 lg:pt-32 xl:space-y-0 xl:flex-row xxl:px-52">
             <div className="grid grid-cols-2 gap-8 xs:gap-10 md:gap-0 md:flex xl:space-x-6">
                 {footerLinks.map(({ title, links }) => (
                     <FooterLinks key={title} title={title} links={links} />
