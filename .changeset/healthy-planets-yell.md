6bf1e930c4ea87e093bcbc40468a0da90ad723bc
Paolo Guerra <pg.guerrap@proton.me>
2023-10-23 17:18:20 +0200

    feat(frontend): add new error icon, add join discord button (#448)
    * feat(frontend): add new error icon, add join discord button
    
    * docs: add changeset

---
+++ .changeset/healthy-planets-yell.md
@@ -0,0 +1,5 @@
+---
+"@carrot-kpi/host-frontend": patch
+---
+
+Add new icon to permissioned mode warning modal, add join discord button
--- packages/frontend/src/components/permissioned/index.tsx
+++ packages/frontend/src/components/permissioned/index.tsx
@@ -4,6 +4,7 @@ import ErrorIcon from "../../icons/error";
 import { Button, Typography } from "@carrot-kpi/ui";
 import { useAccount } from "wagmi";
 import WalletDisconnected from "../../icons/wallet-disconnected";
+import { DISCORD_LINK } from "../../constants";
 
 interface PermissionedProps {
     onBack: () => void;
@@ -17,10 +18,9 @@ export const Permissioned = ({ onBack }: PermissionedProps) => {
             <div className="h-fit flex flex-col gap-4 items-center p-8 max-w-lg rounded-xl border border-black dark:border-white bg-white dark:bg-black mx-4">
                 {address ? (
                     <>
-                        <ErrorIcon className="w-40 h-40 fill-orange stroke-orange" />
+                        <ErrorIcon className="w-52 h-52" />
                         <Typography
-                            variant="xl"
-                            weight="bold"
+                            variant="h4"
                             className={{ root: "text-center" }}
                         >
                             {t("permissioned.mode.title")}
@@ -32,9 +32,23 @@ export const Permissioned = ({ onBack }: PermissionedProps) => {
                         >
                             {t("permissioned.mode.description")}
                         </Typography>
-                        <Button size="small" onClick={onBack}>
-                            {t("permissioned.mode.back")}
-                        </Button>
+                        <div className="flex gap-4">
+                            <Button
+                                size="small"
+                                variant="secondary"
+                                onClick={onBack}
+                            >
+                                {t("permissioned.mode.back")}
+                            </Button>
+                            <Button
+                                size="small"
+                                href={DISCORD_LINK}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                {t("permissioned.mode.discord")}
+                            </Button>
+                        </div>
                     </>
                 ) : (
                     <>
