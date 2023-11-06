556f2ac61a9c2a9776c29071627e8039e10272e8
luzzifoss <fedeluzzi00@gmail.com>
2023-11-06 11:39:58 +0000

    feat(frontend): add e2e tests setup with playwright and synpress

---
+++ packages/frontend/playwright.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+    timeout: 60000,
+    testDir: "./e2e",
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : 1,
+    reporter: [
+        ["list", { printSteps: true }],
+        ["html", { open: "always" }],
+    ],
+    use: {
+        headless: false,
+        baseURL: "http://localhost:3000/#/?chain=scroll+sepolia",
+        trace: "on-first-retry",
+    },
+    projects: [
+        {
+            name: "chromium",
+            use: {
+                ...devices["Desktop Chrome"],
+                viewport: { width: 1920, height: 1080 },
+            },
+        },
+        {
+            name: "firefox",
+            use: { ...devices["Desktop Firefox"] },
+        },
+        {
+            name: "webkit",
+            use: { ...devices["Desktop Safari"] },
+        },
+    ],
+    webServer: {
+        command: "yarn start:staging",
+        url: "http://127.0.0.1:3000",
+        reuseExistingServer: !process.env.CI,
+    },
+});
