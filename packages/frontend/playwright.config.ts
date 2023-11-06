75bb0017442b24ab73f53121c62f707b64bef1fc
luzzifoss <fedeluzzi00@gmail.com>
2023-11-06 15:43:33 +0000

    chore(frontend): increase timeout for web server start

--- packages/frontend/playwright.config.ts
+++ packages/frontend/playwright.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
     ],
     webServer: {
         command: "yarn start:staging",
+        timeout: 120 * 1_000,
         url: "http://127.0.0.1:3000",
         reuseExistingServer: !process.env.CI,
     },
