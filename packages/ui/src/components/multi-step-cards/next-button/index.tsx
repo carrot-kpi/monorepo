e4df459ed6a2d96456720649e91aa40ef0b11aa6
Paolo Guerra <pg.guerrap@proton.me>
2023-11-20 17:27:37 +0100

    Fix markdown styles, next step button customization (#519)
    * fix(ui): add missing font styles for markdown
    
    * fix(ui): make NextStepButton customizable
    
    * docs: add changeset

--- packages/ui/src/components/multi-step-cards/next-button/index.tsx
+++ packages/ui/src/components/multi-step-cards/next-button/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import type { ReactElement } from "react";
 import ChevronDown from "../../../icons/chevron-down";
 import { Button, type ButtonProps } from "../../button";
+import { mergedCx } from "../../../utils/components";
 
 export type NextButtonProps = ButtonProps;
 
 export const NextStepButton = ({
     children,
+    className,
     ...rest
 }: NextButtonProps): ReactElement => {
     return (
@@ -17,8 +19,21 @@ export const NextStepButton = ({
                 className={{
                     // FIXME: find out why important is needed, fix
                     // the root cause and remove it
-                    root: "cui-w-44 cui-h-20 !cui-rounded-3xl cui-bg-orange",
-                    contentWrapper: "cui-pt-3",
+                    // FIXME: some styles don't take effect when the component
+                    // is used externally
+                    root: mergedCx(
+                        [
+                            "cui-w-44",
+                            "cui-h-20",
+                            "!cui-rounded-3xl",
+                            "cui-bg-orange",
+                        ],
+                        className?.root,
+                    ),
+                    contentWrapper: mergedCx(
+                        ["cui-pt-3"],
+                        className?.contentWrapper,
+                    ),
                 }}
             >
                 <div className="cui-flex cui-flex-col cui-items-center">
