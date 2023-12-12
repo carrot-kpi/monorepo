ba66a504804cac615528cec202f10ab98511eaa5
luzzifoss <fedeluzzi00@gmail.com>
2023-12-12 11:45:51 +0100

    chore(react): update size limit script

--- packages/react/.size-limit.js
+++ packages/react/.size-limit.js
@@ -1,12 +1,13 @@
 const path = require("path");
+const { fileURLToPath } = require("url");
 
 module.exports = [
     {
-        path: path.join(path.resolve("./"), "./dist/es/**/*.mjs"),
+        path: path.join(__dirname, "./dist/es/**/*.mjs"),
         limit: "80 KB",
     },
     {
-        path: path.join(path.resolve("./"), "./dist/cjs/**/*.cjs"),
+        path: path.join(__dirname, "./dist/cjs/**/*.cjs"),
         limit: "80 KB",
     },
 ];
