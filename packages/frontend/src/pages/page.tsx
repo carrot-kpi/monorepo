b429647db438c472c3a2f578804e2ed75f9ef1bb
Paolo Guerra <pg.guerrap@proton.me>
2023-12-18 15:54:01 +0100

    feat(frontend): remove watch kpi token with data hook from page

--- packages/frontend/src/pages/page.tsx
+++ packages/frontend/src/pages/page.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from "react";
-import { KPITokenPage, useWatchKPIToken } from "@carrot-kpi/react";
+import {
+    KPITokenPage,
+    useKPIToken,
+    useResolvedKPIToken,
+} from "@carrot-kpi/react";
 import { useLocation, useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { ErrorFeedback, Loader } from "@carrot-kpi/ui";
@@ -14,9 +18,12 @@ export const Page = () => {
     const addTransaction = useAddTransaction();
     const { blacklistedKPITokens } = useBlacklistedTokens();
 
-    const watchedKPITokenWithData = useWatchKPIToken({
-        kpiTokenOrAddress: state?.kpiToken || address,
+    const { kpiToken } = useKPIToken({
+        kpiTokenAddress: state?.kpiToken || address,
         blacklisted: blacklistedKPITokens,
+    });
+    const { resolvedKPIToken } = useResolvedKPIToken({
+        kpiToken: kpiToken || undefined,
     });
 
     useEffect(() => {
@@ -29,7 +36,7 @@ export const Page = () => {
         <Layout navbarBgColor="orange">
             <div className="h-fit flex-grow bg-grid-light bg-left-top bg-orange">
                 <KPITokenPage
-                    kpiToken={watchedKPITokenWithData}
+                    kpiToken={resolvedKPIToken}
                     i18n={i18n}
                     fallback={
                         <div className="h-screen py-20 text-black flex justify-center items-center">
