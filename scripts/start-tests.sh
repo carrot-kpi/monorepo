41446140ef0a7d9b9e72fd51a1ee4f596e1e7ad7
luzzifoss <fedeluzzi00@gmail.com>
2023-12-19 20:42:05 +0100

    chore: remove --exit-code-from flag

--- scripts/start-tests.sh
+++ scripts/start-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker compose -f docker-compose.e2e.yaml \
-    run --build --exit-code-from synpress \
+    run --build synpress \
     -e INFURA_PROJECT_ID \
     -e WALLETCONNECT_PROJECT_ID \
     -e FATHOM_SITE_ID \
