33246d092aa998483fe40825925286526a9eb6b5
luzzifoss <fedeluzzi00@gmail.com>
2023-12-19 20:48:01 +0100

    chore: update docker compose command

--- scripts/start-tests.sh
+++ scripts/start-tests.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-docker compose -f docker-compose.e2e.yaml \
-    run --build synpress \
-    -e INFURA_PROJECT_ID \
-    -e WALLETCONNECT_PROJECT_ID \
-    -e FATHOM_SITE_ID \
-    -e FATHOM_API_KEY
+docker compose -f docker-compose.e2e.yaml run --build -e INFURA_PROJECT_ID -e WALLETCONNECT_PROJECT_ID -e FATHOM_SITE_ID -e FATHOM_API_KEY synpress
