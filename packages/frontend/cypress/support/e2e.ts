30a877ee41e43b101885190513c9aebd57810c0d
luzzifoss <fedeluzzi00@gmail.com>
2023-12-20 14:39:22 +0100

    chore: updates

--- packages/frontend/cypress/support/e2e.ts
+++ packages/frontend/cypress/support/e2e.ts
@@ -2,3 +2,15 @@
 
 import "./commands";
 import "@synthetixio/synpress/support";
+
+console.log("setting up metamask");
+
+if (process.env.CI) {
+    before(() => {
+        cy.setupMetamask(
+            "test test test test test test test test test test test junk",
+            "mumbai",
+            "test-password",
+        );
+    });
+}
