33f0bbcd37029015d76b6d0fd4cca7f393e50026
luzzifoss <fedeluzzi00@gmail.com>
2023-12-20 12:40:54 +0100

    chore: update dockerfile and test

--- packages/frontend/cypress/e2e/connect-wallet.cy.ts
+++ packages/frontend/cypress/e2e/connect-wallet.cy.ts
@@ -11,7 +11,9 @@ describe("Connect with Metamask", () => {
         cy.get('[data-testid="connect-wallet-button"]').eq(1).click();
         cy.get('[data-testid="metaMask-wallet-button"]').eq(1).click();
 
-        cy.acceptMetamaskAccess();
+        cy.acceptMetamaskAccess().then((connected) => {
+            expect(connected).to.be.true;
+        });
 
         cy.get('[data-testid="profile-avatar-button"]')
             .eq(1)
