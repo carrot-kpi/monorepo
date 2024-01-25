184091957a027b49a8c8c8b41fdc293fce11c23d
Federico Luzzi <federico.luzzi@proton.me>
2024-01-25 14:11:27 +0000

    chore: rename data uploader service references to data manager (#574)
    * chore: rename data uploader service references to data manager
    
    * chore(react): fix data manager api path
    
    ---------
    
    Co-authored-by: luzzifoss <fedeluzzi00@gmail.com>

---
+++ .changeset/silver-walls-hammer.md
@@ -0,0 +1,8 @@
+---
+"@carrot-kpi/shared-state": minor
+"@carrot-kpi/host-frontend": minor
+"@carrot-kpi/react": minor
+"@carrot-kpi/sdk": minor
+---
+
+Rename data uploader service references to data manager
--- packages/shared-state/src/reducers/auth/actions/index.ts
+++ packages/shared-state/src/reducers/auth/actions/index.ts
@@ -1,5 +1,3 @@
 import { createAction } from "@reduxjs/toolkit";
 
-export const setDataUploaderJWT = createAction<string>(
-    "auth/setDataUploaderJWT",
-);
+export const setDataManagerJWT = createAction<string>("auth/setDataManagerJWT");
